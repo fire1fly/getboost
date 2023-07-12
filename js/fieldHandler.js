772babe6d77f57e7f45e0290d89e802dde600cc4
fire1fly <danyseidev@gmail.com>
2023-07-12 16:51:15 +0800

    chore: clean code

--- js/fieldHandler.js
+++ js/fieldHandler.js
@@ -3,27 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
   fields = [...fields].filter(element => !element.closest('.m'));
 
-  console.log(fields);
-
-  alert("test 4");
-
-  function getPosition(el) {
-    let x = 0, y = 0, n = true;
-
-    do {
-      if (n) {
-        x += el.offsetLeft || 0
-        y += el.offsetTop || 0
-        n = false
-      } else if (getComputedStyle(el).position === "relative") {
-        n = true
-      }
-      el = el.parentElement;
-    } while (el != null && (el.tagName || '').toLowerCase() !== 'html');
-
-    return {x: parseInt(x, 10), y: parseInt(y, 10)};
-  }
-
   function handleField(e) {
     const input = e.target.closest("INPUT");
     const topOffsetInput = input.getBoundingClientRect().top;
@@ -35,15 +14,6 @@ document.addEventListener("DOMContentLoaded", function() {
         behavior: 'smooth'
       });
     }
-    // input.scrollIntoView({
-    //   behavior: 'smooth',
-    //   block: 'center'
-    // });
-    // const offset = (getPosition(input).y - 200);
-    // window.scrollTo({
-    //   top: offset,
-    //   behavior: 'smooth'
-    // });
   }
 
   if (window.matchMedia("only screen and (max-width: 991px)").matches) {
