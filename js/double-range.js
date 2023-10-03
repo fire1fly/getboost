2f2f563edfec029b8a72270f145832fb1a6fe421
fire1fly <danyseidev@gmail.com>
2023-10-04 05:07:48 +0800

    refactor: add select validate, edit double range structure

--- js/double-range.js
+++ js/double-range.js
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const trackFillBg = "#1B1930";
 
   class DoubleRange {
-    constructor(item, isExtra = false) {
+    constructor(item, options) {
       this.$range1 = item.querySelector(".range-1");
       this.$range2 = item.querySelector(".range-2");
       this.$input1 = item.querySelector(".value-1");
       this.$input2 = item.querySelector(".value-2");
       this.$track = item.querySelector(".double-range-track");
-      this.isExtra = isExtra;
-      this.minGap = 1;
+      this.isExtra = options.isExtra;
+      this.minGap = options.minGap;
 
       if (this.isExtra) {
-        this.$select1 = item.querySelector(".double-range-select-1");
-        this.$select2 = item.querySelector(".double-range-select-2");
+        this.$select1 = item.querySelector(".double-range-select-1 .dd-input");
+        this.$select2 = item.querySelector(".double-range-select-2 .dd-input");
+        this.selectValueList1 = item.querySelectorAll(".double-range-select-1 .dd-item");
+        this.selectValueList2 = item.querySelectorAll(".double-range-select-2 .dd-item");
         this.$refValueList1 = item.querySelectorAll(".double-range-refvalue-1 .double-range-refvalue");
         this.$refValueList2 = item.querySelectorAll(".double-range-refvalue-2 .double-range-refvalue");
       }
@@ -26,21 +28,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     setup() {
-      this.handleSlide1 = this.handleSlide1.bind(this);
-      this.handleSlide2 = this.handleSlide2.bind(this);
-      this.$range1.addEventListener("input", this.handleSlide1);
-      this.$range2.addEventListener("input", this.handleSlide2);
-
-      this.handleInput1 = this.handleInput1.bind(this);
-      this.handleInput2 = this.handleInput2.bind(this);
-      this.$input1.addEventListener("input", this.handleInput1);
-      this.$input2.addEventListener("input", this.handleInput2);
+      this.$range1.addEventListener("input", () => this.handleSlide1());
+      this.$range2.addEventListener("input", () => this.handleSlide2());
+      this.$input1.addEventListener("input", () => this.handleInput1());
+      this.$input2.addEventListener("input", () => this.handleInput2());
 
       if (this.isExtra) {
-        this.handleSelect1 = this.handleSelect1.bind(this);
-        this.handleSelect2 = this.handleSelect2.bind(this);
-        this.$select1.addEventListener("change", this.handleSelect1);
-        this.$select2.addEventListener("change", this.handleSelect2);
+        this.$select1.addEventListener("change", () => this.handleSelect1());
+        this.$select2.addEventListener("change", () => this.handleSelect2());
       }
     }
 
@@ -50,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       this.$input1.value = this.$range1.value;
       if (this.isExtra) {
-        this.$select1.value = this.$range1.value;
+        this.validateSelect1(this.$range1.value);
         this.handeRefValues1();
       }
       this.fillTrack();
@@ -62,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       this.$input2.value = this.$range2.value;
       if (this.isExtra) {
-        this.$select2.value = this.$range2.value;
+        this.validateSelect2(this.$range2.value);
         this.handeRefValues2();
       }
       this.fillTrack();
@@ -73,12 +68,13 @@ document.addEventListener("DOMContentLoaded", function () {
         (parseInt(this.$input1.value) <= parseInt(this.$range2.value))) {
         this.$range1.value = this.$input1.value;
         if (this.isExtra) {
+          this.validateSelect1(this.$input1.value);
           this.$select1.value = this.$input1.value;
         }
       } else {
         this.$range1.value = this.$range2.min;
         if (this.isExtra) {
-          this.$select1.value = this.$range2.value;
+          // this.$select1.value = this.$range2.min;
           this.handeRefValues1();
         }
       }
@@ -89,16 +85,43 @@ document.addEventListener("DOMContentLoaded", function () {
         (parseInt(this.$input2.value) >= parseInt(this.$range1.value))) {
         this.$range2.value = this.$input2.value;
         if (this.isExtra) {
-          this.$select2.value = this.$input2.value;
+          this.validateSelect2(this.$input2.value)
+          // this.$select2.value = this.$input2.value;
         }
       } else {
         this.$range2.value = this.$range1.max;
         if (this.isExtra) {
-          this.$select2.value = this.$range1.value;
+          // this.$select2.value = this.$range1.value;
           this.handeRefValues2();
         }
       }
       this.fillTrack();
+    }
+
+    validateSelect1(prevalue) {
+      let values = [];
+      let prevalueInt = parseInt(prevalue);
+      this.selectValueList1.forEach(item => values.push(parseInt(item.dataset.value)));
+      this.selectValueList1.forEach((item, i) => {
+        if (prevalueInt >= values[i] && prevalueInt < values[i + 1]) {
+          this.$select1.value = values[i];
+        }
+      });
+      this.$input1.value = prevalueInt;
+      this.$range1.value = prevalueInt;
+    }
+
+    validateSelect2(prevalue) {
+      let values = [];
+      let prevalueInt = parseInt(prevalue);
+      this.selectValueList2.forEach(item => values.push(parseInt(item.dataset.value)));
+      this.selectValueList2.forEach((item, i) => {
+        if (prevalueInt >= values[i] && prevalueInt < values[i + 1]) {
+          this.$select2.value = values[i];
+        }
+      });
+      this.$input2.value = prevalueInt;
+      this.$range2.value = prevalueInt;
     }
 
     handleSelect1() {
@@ -135,13 +158,13 @@ document.addEventListener("DOMContentLoaded", function () {
         item.hidden = true;
         if (
           (
-            parseInt(this.$input1.value) >= values[i]
-            && parseInt(this.$input1.value) < values[i + 1]
+            parseInt(this.$range1.value) >= values[i]
+            && parseInt(this.$range1.value) < values[i + 1]
             ||
-            parseInt(this.$input1.value) >= values[values.length - 1]
+            parseInt(this.$range1.value) >= values[values.length - 1]
             && values[i + 1] === undefined
             ||
-            parseInt(this.$input1.value) < values[0]
+            parseInt(this.$range1.value) < values[0]
             && values[i - 1] === undefined
           )
         ) {
@@ -158,13 +181,13 @@ document.addEventListener("DOMContentLoaded", function () {
         item.hidden = true;
         if (
           (
-            parseInt(this.$input2.value) >= values[i]
-            && parseInt(this.$input2.value) < values[i + 1]
+            parseInt(this.$range2.value) >= values[i]
+            && parseInt(this.$range2.value) < values[i + 1]
             ||
-            parseInt(this.$input2.value) >= values[values.length - 1]
+            parseInt(this.$range2.value) >= values[values.length - 1]
             && values[i + 1] === undefined
             ||
-            parseInt(this.$input2.value) < values[0]
+            parseInt(this.$range2.value) < values[0]
             && values[i - 1] === undefined
           )
         ) {
@@ -183,7 +206,10 @@ document.addEventListener("DOMContentLoaded", function () {
   let doubleRanges = document.querySelectorAll(".double-range");
 
   doubleRanges.forEach(item => {
-    const doubleRange = new DoubleRange(item, item.dataset.rangeType === 'extra' ? true : false);
+    const doubleRange = new DoubleRange(item, {
+      isExtra: item.dataset.rangeType === 'extra' ? true : false,
+      minGap: 1
+    });
   });
 
 });
